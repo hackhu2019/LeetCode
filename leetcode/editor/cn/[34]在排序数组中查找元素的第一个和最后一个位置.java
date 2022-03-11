66a2ac3e33ec4d4649a16ac68f98ca4ad093dc70
hack-hu <hack_hu@163.com>
2022-03-11 15:00:09 +0800

    add: 二分查找系列

---
+++ leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,76 @@
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。 
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。 
+//
+// 进阶： 
+//
+// 
+// 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4] 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1] 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [], target = 0
+//输出：[-1,-1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// nums 是一个非递减数组 
+// -10⁹ <= target <= 10⁹ 
+// 
+// Related Topics 数组 二分查找 👍 1518 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        return new int[]{searchLeftBound(nums, target), searchRightBound(nums, target)};
+    }
+
+    private int searchLeftBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left >= nums.length || nums[left] != target ? -1 : left;
+    }
+
+    private int searchRightBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right < 0 || nums[right] != target ? -1 : right;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
