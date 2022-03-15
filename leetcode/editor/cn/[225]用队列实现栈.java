7eb98cd25f835909fb3f415d4fa83ffdc15d1155
hack-hu <hack_hu@163.com>
2022-03-15 09:58:56 +0800

    add: 栈与队列

---
+++ leetcode/editor/cn/[225]用队列实现栈.java
@@ -0,0 +1,103 @@
+//请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+//
+// 实现 MyStack 类：
+//
+//
+// void push(int x) 将元素 x 压入栈顶。
+// int pop() 移除并返回栈顶元素。
+// int top() 返回栈顶元素。
+// boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+//
+//
+//
+//
+// 注意：
+//
+//
+// 你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+// 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+//
+//
+//
+//
+// 示例：
+//
+//
+//输入：
+//["MyStack", "push", "push", "top", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//输出：
+//[null, null, null, 2, 2, false]
+//
+//解释：
+//MyStack myStack = new MyStack();
+//myStack.push(1);
+//myStack.push(2);
+//myStack.top(); // 返回 2
+//myStack.pop(); // 返回 2
+//myStack.empty(); // 返回 False
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= x <= 9
+// 最多调用100 次 push、pop、top 和 empty
+// 每次调用 pop 和 top 都保证栈不为空
+//
+//
+//
+//
+// 进阶：你能否仅用一个队列来实现栈。
+// Related Topics 栈 设计 队列 👍 456 👎 0
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 栈，后进先出
+class MyStack {
+    private Queue<Integer> queue;
+    private Integer topElement;
+    public MyStack() {
+        queue = new LinkedList<>();
+        topElement = null;
+    }
+
+    public void push(int x) {
+        queue.offer(x);
+        topElement = x;
+    }
+
+    public int pop() {
+        int size = queue.size();
+        while (size > 2) {
+            queue.offer(queue.poll());
+            size--;
+        }
+        topElement = queue.peek();
+        queue.offer(queue.poll());
+        return queue.poll();
+    }
+
+    public int top() {
+        return topElement;
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[232]用栈实现队列.java
@@ -0,0 +1,106 @@
+//请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+//
+// 实现 MyQueue 类：
+//
+//
+// void push(int x) 将元素 x 推到队列的末尾
+// int pop() 从队列的开头移除并返回元素
+// int peek() 返回队列开头的元素
+// boolean empty() 如果队列为空，返回 true ；否则，返回 false
+//
+//
+// 说明：
+//
+//
+// 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法
+//的。
+// 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：
+//["MyQueue", "push", "push", "peek", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//输出：
+//[null, null, null, 1, 1, false]
+//
+//解释：
+//MyQueue myQueue = new MyQueue();
+//myQueue.push(1); // queue is: [1]
+//myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+//myQueue.peek(); // return 1
+//myQueue.pop(); // return 1, queue is [2]
+//myQueue.empty(); // return false
+//
+//
+//
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= x <= 9
+// 最多调用 100 次 push、pop、peek 和 empty
+// 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+//
+// Related Topics 栈 设计 队列 👍 590 👎 0
+
+
+import java.util.Stack;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyQueue {
+    private final Stack<Integer> stack1;
+    private final Stack<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
